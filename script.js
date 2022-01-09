139407f675c788534b52cb57699f85a75d01fd56
Ilona Drobot <drobotilonna@gmail.com>
2022-01-09 15:19:43 +0200

    homework30

---
+++ script.js
@@ -0,0 +1,11 @@
+let first = prompt("Введите ваш возраст")
+let second = prompt("Какой сейчас год")
+
+console.log(first)
+console.log(second)
+
+let c = Number(first)
+let n = Number(second)
+
+ 
+ console.log(n - c )
