eb967f8175282190da9a907957682a579dd99c1a
Ilona Drobot <drobotilonna@gmail.com>
2022-01-19 18:12:00 +0200

    homework33

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let amount =
+let amount = Number
  
  console.log(amount)
  for(let i = 1; i <= 21; i = i + 2 ){
      console.log ("hello", i)
  }
+
+ for(let i = 1; i <= 100; i = i +1 ){
+    console.log ("hello", i)
+}
+
+
